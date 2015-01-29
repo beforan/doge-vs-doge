fc7f2be35a3cb9d0e72407fe433af3478de63397
beforan <beforan86@gmail.com>
2015-01-29 11:33:08 +0000

    Create settings.lua

---
+++ settings.lua
@@ -0,0 +1,27 @@
+Settings = Class {	
+	LiteDoge = {
+		Controls = {
+			Up = "up",
+			Down = "down",
+			Left = "left",
+			Right = "right",
+			Action = "rctrl",
+			Map	= "/",
+		},
+	},
+	
+	DarkDoge = {
+		Controls = {
+			Up = "w",
+			Down = "s",
+			Left = "a",
+			Right = "d",
+			Action = "lalt",
+			Map = "z",
+		},
+	},
+	
+	Map = {
+		LaunchPadVisibleAtStart = false,
+	},
+}
